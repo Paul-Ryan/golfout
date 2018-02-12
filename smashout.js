945829572e242c926fadea77e93580809f018c4d
Paul <paul.ryan.iii@gmail.com>
2018-02-12 15:16:52 -0800

    Add event listeners for keypresses

--- smashout.js
+++ smashout.js
@@ -8,10 +8,32 @@ document.addEventListener("DOMContentLoaded", () => {
   // const game = new Game(ctx);
 });
 
+
 const canvasEl = document.getElementById("smashout");
 const ctx = canvasEl.getContext("2d");
 const game = new Game(ctx, canvasEl);
 
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+let rightPressed = false;
+let leftPressed = false;
+
+function keyDownHandler(e) {
+  if (e.keyCode === 37) {
+    leftPressed = true;
+  } else if (e.keyCode === 39) {
+    rightPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode === 37) {
+    leftPressed = false;
+  } else if (e.keyCode === 39) {
+    rightPressed = false;
+  }
+}
 
 function draw() {
   game.draw(ctx);

1cb8927c26ff5f4256cf08e18871459edb12e3a8
Paul <paul.ryan.iii@gmail.com>
2018-02-12 11:50:50 -0800

    Put ball position in ball class

--- smashout.js
+++ smashout.js
@@ -5,42 +5,23 @@ import Paddle from './lib/paddle.js';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const game = new Game(ctx);
-  // new GameView(game, ctx);
+  // const game = new Game(ctx);
 });
 
 const canvasEl = document.getElementById("smashout");
 const ctx = canvasEl.getContext("2d");
-const ball = new Ball();
-const paddle = new Paddle();
-
-let ballPos = [canvasEl.width/2, canvasEl.height - 10];
-let movement = [1.4, -2];
-// let dx = 1.2;
-// let dy = -2;
+const game = new Game(ctx, canvasEl);
 
 let paddlePos = [canvasEl.width/2, canvasEl.height - 20];
-
+let paddle = new Paddle();
 function draw() {
-  ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-  ball.drawBall(ballPos);
+  // ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  // ball.drawBall();
+  // ball.moveBall();
   paddle.drawPaddle(paddlePos);
+  game.draw(ctx);
 
-  ballPos[0] += movement[0];
-  ballPos[1] += movement[1];
-
-  if (ballPos[1] < ball.radius) {
-    movement[1] *= -1;
-  }
-  if (ballPos[0] > canvasEl.width - ball.radius) {
-    movement[0] *= -1;
-  }
-  if (ballPos[1] > canvasEl.height - ball.radius) {
-    movement[1] *= -1;
-  }
-  if (ballPos[0] < ball.radius) {
-    movement[0] *= -1;
-  }
 }
 
 setInterval(draw, 1000/60);
+// setInterval(draw, 2000);

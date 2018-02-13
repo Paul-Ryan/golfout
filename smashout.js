cbabf99352a80e8efac78f1e7322c61eb5101418
Paul <paul.ryan.iii@gmail.com>
2018-02-13 10:21:52 -0800

    Game over when ball hits hole

--- smashout.js
+++ smashout.js
@@ -4,30 +4,18 @@ import Paddle from './lib/paddle.js';
 // import GameView from './lib/game_view.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // const game = new Game(ctx);
+  const canvasEl = document.getElementById("smashout");
+  const ctx = canvasEl.getContext("2d");
+  const game = new Game(ctx, canvasEl);
+  game.addListners();
+  game.start();
 });
-
-const canvasEl = document.getElementById("smashout");
-const ctx = canvasEl.getContext("2d");
-const game = new Game(ctx, canvasEl);
-
-document.addEventListener("keydown", game.keyDownHandler);
-document.addEventListener("keyup", game.keyUpHandler);
-document.addEventListener("mousemove", game.mouseMoveHandler, false);
-
-function draw() {
-  game.draw(ctx);
-}
-
-
-setInterval(draw, 1000/60);
-// setInterval(draw, 2000);
 
 
 // todo:
 
 // X adjust bounce based on paddle contact point
-// add hole
+// X add hole
 // add victory/loss messages
 // learn about requestAnimationFrame
 // improve physics of ball

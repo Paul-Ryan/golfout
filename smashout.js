93671f1a3ba3abdaf724feef0b2ccef47a0da802
Paul <paul.ryan.iii@gmail.com>
2018-02-12 17:05:56 -0800

    add object for other stuff to inherit from

--- smashout.js
+++ smashout.js
@@ -3,37 +3,16 @@ import Ball from './lib/ball.js';
 import Paddle from './lib/paddle.js';
 // import GameView from './lib/game_view.js';
 
-
 document.addEventListener("DOMContentLoaded", () => {
   // const game = new Game(ctx);
 });
-
 
 const canvasEl = document.getElementById("smashout");
 const ctx = canvasEl.getContext("2d");
 const game = new Game(ctx, canvasEl);
 
-document.addEventListener("keydown", keyDownHandler);
-document.addEventListener("keyup", keyUpHandler);
-
-let rightPressed = false;
-let leftPressed = false;
-
-function keyDownHandler(e) {
-  if (e.keyCode === 37) {
-    leftPressed = true;
-  } else if (e.keyCode === 39) {
-    rightPressed = true;
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.keyCode === 37) {
-    leftPressed = false;
-  } else if (e.keyCode === 39) {
-    rightPressed = false;
-  }
-}
+document.addEventListener("keydown", game.keyDownHandler);
+document.addEventListener("keyup", game.keyUpHandler);
 
 function draw() {
   game.draw(ctx);
